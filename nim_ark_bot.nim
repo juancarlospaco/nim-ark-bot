8dc3156b637ac3eb58fb3d47c870fa5abeeba2ac
juancarlospaco <juancarlospaco@gmail.com>
2018-08-01 05:52:30 -0300

    Add Geo POI Sharing

--- nim_ark_bot.nim
+++ nim_ark_bot.nim
@@ -2,6 +2,8 @@ import
   asyncdispatch, httpclient, logging, json, options, ospaths, osproc, parsecfg,
   strformat, strutils, terminal, times, random, posix
 import telebot  # nimble install telebot / https://nimble.directory/pkg/telebot / Version 0.3.3
+import openexchangerates  # nimble install openexchangerates  https://github.com/juancarlospaco/nim-openexchangerates
+
 
 const
   about_texts = fmt"""*Nim Telegram Bot* 🤖
@@ -35,6 +37,17 @@ let
   server_cmd_lspci = parseBool(config_ini.getSectionValue("linux_server_admin_commands", "lspci"))
   server_cmd_public_ip = parseBool(config_ini.getSectionValue("linux_server_admin_commands", "public_ip"))
 
+  cmd_bash0 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin0_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin0_command"))
+  cmd_bash1 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin1_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin1_command"))
+  cmd_bash2 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin2_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin2_command"))
+  cmd_bash3 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin3_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin3_command"))
+  cmd_bash4 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin4_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin4_command"))
+  cmd_bash5 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin5_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin5_command"))
+  cmd_bash6 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin6_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin6_command"))
+  cmd_bash7 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin7_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin7_command"))
+  cmd_bash8 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin8_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin8_command"))
+  cmd_bash9 = (name: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin9_name"), command: config_ini.getSectionValue("bash_plugin_commands", "bash_plugin9_command"))
+
   ark_cmd_saveworld    = parseBool(config_ini.getSectionValue("ark_commands", "saveworld"))
   ark_cmd_listplayers  = parseBool(config_ini.getSectionValue("ark_commands", "listplayers"))
   ark_cmd_getchat      = parseBool(config_ini.getSectionValue("ark_commands", "getchat"))
@@ -47,12 +60,19 @@ let
   ark_cmd_destroywilddinos = parseBool(config_ini.getSectionValue("ark_commands", "destroywilddinos"))
   ark_bot_start_notify = parseBool(config_ini.getSectionValue("ark_commands", "bot_start_notify"))
 
+  cmd_geo0 = (lat: parseFloat(config_ini.getSectionValue("geo_location_sharing", "geo_plugin0_lat")), lon: parseFloat(config_ini.getSectionValue("geo_location_sharing", "geo_plugin0_lon")))
+
   rcon_ip   = config_ini.getSectionValue("rcon_server", "ip")
   rcon_port = config_ini.getSectionValue("rcon_server", "port")
   rcon_pass = config_ini.getSectionValue("rcon_server", "password")
   rcon_cmd = fmt"mcrcon -c -H {rcon_ip} -P {rcon_port} -p {rcon_pass} "
 
   polling_interval: int8 = parseInt(config_ini.getSectionValue("", "polling_interval")).int8
+  oer_api_key = config_ini.getSectionValue("openexchangerates", "api_key")
+  oer_currenc = config_ini.getSectionValue("openexchangerates", "currencies").split(",")
+  oer_round = parseBool(config_ini.getSectionValue("openexchangerates", "round_prices"))
+  oer_client = AsyncOER(timeout: 3, api_key: oer_api_key, base: "USD", local_base: "",
+                        round_float: oer_round, prettyprint: false, show_alternative: true)
 
 var counter: int
 
@@ -69,6 +89,24 @@ template handlerizer(body: untyped): untyped =
     except Exception:
       discard
   result = cb
+
+template handlerizerLocation(body: untyped): untyped =
+  proc cb(e: Command) {.async.} =
+    inc counter
+    body
+    let
+      geo_uri = "*GEO URI:* geo:$1,$2    ".format(latitud, longitud)
+      osm_url = "*OSM URL:* https://www.openstreetmap.org/?mlat=$1&mlon=$2".format(latitud, longitud)
+    var
+      msg = newMessage(e.message.chat.id,  geo_uri & osm_url)
+      geo_msg = newLocation(e.message.chat.id, longitud, latitud)
+    msg.disableNotification = true
+    geo_msg.disableNotification = true
+    msg.parseMode = "markdown"
+    discard bot.send(geo_msg)
+    discard bot.send(msg)
+  result = cb
+
 
 proc public_ipHandler(bot: Telebot): CommandCallback =
   handlerizer():
@@ -104,6 +142,23 @@ proc donateHandler(bot: Telebot): CommandCallback =
 proc modsHandler(bot: Telebot): CommandCallback =
   handlerizer():
     let message = readFile("mods_list.md")
+
+proc dollarHandler(bot: Telebot): CommandCallback =
+  let
+    money_json = waitFor oer_client.latest()      # Updated Prices.
+    names_json = waitFor oer_client.currencies()  # Friendly Names.
+  var dineros = ""
+  for crrncy in money_json.pairs:
+    if crrncy[0] in oer_currenc:
+      dineros.add fmt"*{crrncy[0]}* _{names_json[crrncy[0]]}_: `{crrncy[1]}`,  "
+  handlerizer():
+    let message = dineros
+
+proc geoHandler(bot: Telebot, latitud, longitud: float): CommandCallback =
+  handlerizerLocation():
+    let
+      latitud = latitud
+      longitud = longitud
 
 
 when defined(linux):
@@ -182,6 +237,10 @@ when defined(linux):
         status =  await responz.body                           # await body
         message = fmt"`{status}`"
 
+  proc cmd_bashHandler(bot: Telebot, command: string): CommandCallback =
+    handlerizer():
+      let message = fmt"""`{execCmdEx(command)[0]}`"""
+
 
 proc main*() {.async.} =
   ## Main loop of the bot.
@@ -200,6 +259,8 @@ proc main*() {.async.} =
   if cmd_uptime:   bot.onCommand("uptime", uptimeHandler(bot))
   if cmd_donate:   bot.onCommand("donate", donateHandler(bot))
   if cmd_datetime: bot.onCommand("datetime", datetimeHandler(bot))
+  if oer_api_key != "": bot.onCommand("dollar", dollarHandler(bot))
+  if cmd_geo0.lat != 0.0 and cmd_geo0.lon != 0.0: bot.onCommand("serverlocation", geoHandler(bot, cmd_geo0.lat, cmd_geo0.lon))
 
   when defined(linux):
     if server_cmd_ip:        bot.onCommand("ip", ipHandler(bot))
@@ -221,10 +282,29 @@ proc main*() {.async.} =
     if ark_cmd_destroywilddinos: bot.onCommand("destroywilddinos", destroywilddinosHandler(bot))
 
     if ark_bot_start_notify: echo execCmdEx(rcon_cmd & "'broadcast Ark_Telegram_Bot_Started.'")
-    try:
-      discard nice(19.cint)  # Smooth CPU priority.
-    except Exception:
-      discard
+
+    if cmd_bash0.name != "" and cmd_bash0.command != "":
+      bot.onCommand($cmd_bash0.name, cmd_bashHandler(bot, cmd_bash0.command))
+    if cmd_bash1.name != "" and cmd_bash1.command != "":
+      bot.onCommand($cmd_bash1.name, cmd_bashHandler(bot, cmd_bash1.command))
+    if cmd_bash2.name != "" and cmd_bash2.command != "":
+      bot.onCommand($cmd_bash2.name, cmd_bashHandler(bot, cmd_bash2.command))
+    if cmd_bash3.name != "" and cmd_bash3.command != "":
+      bot.onCommand($cmd_bash3.name, cmd_bashHandler(bot, cmd_bash3.command))
+    if cmd_bash4.name != "" and cmd_bash4.command != "":
+      bot.onCommand($cmd_bash4.name, cmd_bashHandler(bot, cmd_bash4.command))
+    if cmd_bash5.name != "" and cmd_bash5.command != "":
+      bot.onCommand($cmd_bash5.name, cmd_bashHandler(bot, cmd_bash5.command))
+    if cmd_bash6.name != "" and cmd_bash6.command != "":
+      bot.onCommand($cmd_bash6.name, cmd_bashHandler(bot, cmd_bash6.command))
+    if cmd_bash7.name != "" and cmd_bash7.command != "":
+      bot.onCommand($cmd_bash7.name, cmd_bashHandler(bot, cmd_bash7.command))
+    if cmd_bash8.name != "" and cmd_bash8.command != "":
+      bot.onCommand($cmd_bash8.name, cmd_bashHandler(bot, cmd_bash8.command))
+    if cmd_bash9.name != "" and cmd_bash9.command != "":
+      bot.onCommand($cmd_bash9.name, cmd_bashHandler(bot, cmd_bash9.command))
+
+    discard nice(19.cint)  # smooth cpu priority
 
   bot.poll(int32(polling_interval * 1000))
 
